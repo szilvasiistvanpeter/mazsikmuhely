68458f2012661a7823411e0a8f9e3b57a37638f5
Szilvási István Péter <szilvasipeter@protonmail.com>
2026-05-16 09:57:13 +0200

    fix(router)-standardize import and route path formatting in router configuration

--- mazsikmuhely/src/router/index.ts
+++ mazsikmuhely/src/router/index.ts
@@ -1,33 +1,33 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
-import AboutView from "@/views/AboutView.vue";
-import ServiceView from "@/views/ServiceView.vue";
-import ContactView from "@/views/ContactView.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import ServiceView from '@/views/ServiceView.vue'
+import ContactView from '@/views/ContactView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: HomeView,
     },
     {
-      path: "/about",
-      name: "about",
+      path: '/about',
+      name: 'about',
       component: AboutView,
     },
     {
-      path: "/service",
-      name: "service",
+      path: '/service',
+      name: 'service',
       component: ServiceView,
     },
     {
-      path: "/contact",
-      name: "contact",
+      path: '/contact',
+      name: 'contact',
       component: ContactView,
     },
   ],
-});
+})
 
-export default router;
+export default router
