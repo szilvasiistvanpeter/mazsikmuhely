a4c1eb112f24a57c1d8f9adbf8ddc324d1700e51
Szilvási István Péter <szilvasipeter@protonmail.com>
2026-05-16 08:36:34 +0200

    feat(components)-add Hero and Home components with responsive design

---
+++ mazsikmuhely/src/components/Home.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Hero from "@/components/Hero.vue";
+</script>
+
+<template>
+  <Hero
+    title="Precíziós fékrendszerek és alkatrészek"
+    subtitle="Megállítjuk a teljesítményt. Prémium minőség, extrém igénybevételre is."
+  >
+    <template #media>
+      <img src="@/assets/hero.jpeg" alt="Mazsik fékrendszer" />
+    </template>
+  </Hero>
+</template>
